349903ff30ab6f50688e56c177f0118408c50880
km185n123 <km185n123@gmail.com>
2026-03-28 04:20:57 -0500

    add  mock interceptor

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'mock_interceptor.dart';
 
 class DioClient {
   late final Dio dio;
@@ -6,23 +7,12 @@ class DioClient {
   DioClient() {
     dio = Dio(
       BaseOptions(
-        // URL base asumiendo que el json-server corre en localhost.
-        // Para emuladores Android suele ser 10.0.2.2 y para iOS localhost.
         baseUrl: 'http://localhost:3000',
         connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 3),
       ),
     );
 
-    dio.interceptors.add(
-      LogInterceptor(
-        request: true,
-        requestHeader: true,
-        requestBody: true,
-        responseHeader: true,
-        responseBody: true,
-        error: true,
-      ),
-    );
+    dio.interceptors.add(MockInterceptor());
   }
 }
---
+++ lib/core/network/mock_interceptor.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+
+class MockInterceptor extends Interceptor {
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    try {
+      final cleanPath = options.path.startsWith('/')
+          ? options.path.substring(1)
+          : options.path;
+
+      final resourcePath = 'assets/mocks/$cleanPath.json';
+
+      final data = await rootBundle.loadString(resourcePath);
+      final jsonResult = json.decode(data);
+
+      final result = (jsonResult is Map && jsonResult.containsKey(cleanPath))
+          ? jsonResult[cleanPath]
+          : jsonResult;
+
+      await Future.delayed(const Duration(milliseconds: 500));
+
+      return handler.resolve(
+        Response(requestOptions: options, data: result, statusCode: 200),
+      );
+    } catch (e) {
+      print(
+        'MockInterceptor: No se pudo cargar el mock para ${options.path} -> $e',
+      );
+      return handler.next(options);
+    }
+  }
+}
